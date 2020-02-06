2270ccb3d6b8e3b010d537fba5bf7a724ad5609f
Dao Van Man <vanmandao1@gmail.com>
2020-02-06 13:21:49 +0700

    chapter 4: Rails-flavored Ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 module ApplicationHelper
-  def full_title; end
+  def full_title page_title = ""
+    base_title = I18n.t "base_title"
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,10 +3,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
-  test "should get root" do
-    get FILL_IN
-    assert_response FILL_IN
-  end
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -21,5 +17,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
