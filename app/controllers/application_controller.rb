38f269cf8a916f1573ea438b8848d80aac0c2ed4
Dao Van Man <vanmandao1@gmail.com>
2020-02-07 14:18:33 +0700

    chapter 3: Mosthly static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+  around_action :switch_locale
+
+  def switch_locale &action
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
   def hello
     render html: "hello, world!"
